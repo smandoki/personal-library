0882d9353a47c6444730b195502a99629e8e1000
smandoki <80867148+smandoki@users.noreply.github.com>
2022-09-30 15:14:11 +0800

    Reverted previous change
    Reimplemented table, and implemented adding new book and generating
    new table rows

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('#modal-add-book');
 const addBook = document.querySelector('#btn-add-book');
 const form = document.getElementById("form-add-book");
+const table = document.querySelector('table');
 const books = [];
 
 addBook.onclick = function(e) {
@@ -23,6 +24,27 @@ function createBook() {
     );
 
     books.push(book);
+    createRow(book);
+}
+
+function createRow(book) {
+    const row = table.insertRow();
+
+    const title = row.insertCell();
+    const author = row.insertCell();
+    const pages = row.insertCell();
+    const read = row.insertCell();
+    const remove = row.insertCell();
+
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages;
+
+    const readStatus = book.read ? 'read' : 'not read';
+    const color = book.read ? 'green' : 'red';
+
+    read.innerHTML = `<button onclick="toggleStatus()" class="${color}">${readStatus}</button>`;
+    remove.innerHTML = '<button onclick="removeBook();" class="red"><i class="bi bi-trash"></i></button>';
 }
 
 class Book {
