488ca7958ead23109b3474df7eecca92fdc22270
smandoki <80867148+smandoki@users.noreply.github.com>
2022-09-30 14:58:02 +0800

    Removed table
    replacing table with 'cards'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('#modal-add-book');
 const addBook = document.querySelector('#btn-add-book');
 const form = document.getElementById("form-add-book");
+const books = [];
 
 addBook.onclick = function(e) {
     form.reset();
@@ -12,3 +13,23 @@ modal.addEventListener('click', (e) => {
         modal.close();
     }
 });
+
+function createBook() {
+    const book = new Book(
+        form.title.value,
+        form.author.value,
+        form.pages.value,
+        form.read.checked
+    );
+
+    books.push(book);
+}
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
